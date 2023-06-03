9c35e5445284648523872d6dd4c17bc7220991a6
Antônio José Martins Mascarenhas <amartinsmascarenhas@gmail.com>
2023-06-03 14:12:57 -0300

    Update index.js

--- Project All Pisos/all-pisos2.0/src/componentes/rodape/index.js
+++ Project All Pisos/all-pisos2.0/src/componentes/rodape/index.js
@@ -4,7 +4,7 @@ import './style.css'
 export default function Rodape(){
     return(
         <footer>
-            CRIADOR POR <a href="https://www.linkedin.com/in/ant%C3%B4nio-jos%C3%A9-martins-mascarenhas-12598b261/" target="_blank" rel="noreferrer">ANTÔNIO JOSÉ MARTINS MASCARENHAS</a>
+            CRIADO POR <a href="https://www.linkedin.com/in/ant%C3%B4nio-jos%C3%A9-martins-mascarenhas-12598b261/" target="_blank" rel="noreferrer">ANTÔNIO JOSÉ MARTINS MASCARENHAS</a>
             <a className="voltar-topo" href="#topo" title="Voltar ao topo"><img src="/assets/seta-cima.png" alt="Voltar ao topo"/></a>
         </footer>
     );
